a29e96f3a25e66f68ac98c1699d10ad4ec803cbe
Tashiro <g4160hc@gmail.com>
2022-01-25 16:16:01 +0900

    test

--- test/works_web/controllers/user_registration_controller_test.exs
+++ test/works_web/controllers/user_registration_controller_test.exs
@@ -4,39 +4,9 @@ defmodule WorksWeb.UserRegistrationControllerTest do
   import Works.AccountsFixtures
 
   describe "GET /users/register" do
-    test "renders registration page", %{conn: conn} do
-      conn = get(conn, Routes.user_registration_path(conn, :new))
-      response = html_response(conn, 200)
-      assert response =~ "<h1>Register</h1>"
-      assert response =~ "Log in</a>"
-      assert response =~ "Register</a>"
-    end
-
     test "redirects if already logged in", %{conn: conn} do
       conn = conn |> log_in_user(user_fixture()) |> get(Routes.user_registration_path(conn, :new))
       assert redirected_to(conn) == "/"
-    end
-  end
-
-  describe "POST /users/register" do
-    @tag :capture_log
-    test "creates account and logs the user in", %{conn: conn} do
-      email = unique_user_email()
-
-      conn =
-        post(conn, Routes.user_registration_path(conn, :create), %{
-          "user" => valid_user_attributes(email: email)
-        })
-
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
-
-      # Now do a logged in request and assert on the menu
-      conn = get(conn, "/")
-      response = html_response(conn, 200)
-      assert response =~ email
-      assert response =~ "Settings</a>"
-      assert response =~ "Log out</a>"
     end
   end
 end
